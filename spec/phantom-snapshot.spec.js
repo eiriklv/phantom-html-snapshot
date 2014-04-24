d97b49b9b6e27662045e7a674d066b18e8b83f8b
Jacques-Olivier D. Bernier <jdesjardinsbernier@gmail.com>
2014-04-24 15:35:37 -0400

    first working version

---
+++ spec/phantom-snapshot.spec.js
@@ -0,0 +1,11 @@
+describe('unit tests html snapshots', function () {
+    describe('take', function () {
+        it('should return the html from a valid url', function () {
+            //TODO.. Need to test that phantomjs is called correctly
+        });
+
+        it('should return an InvalidUrlError if an invalid url is provided', function () {
+            //TODO.. Need to test that phantomjs is called correctly
+        });
+    });
+});
